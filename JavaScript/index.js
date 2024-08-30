cb19880a052b168a80da829c57b70478ccc2b78d
malory171 <malory171@gmail.com>
2024-08-30 21:01:15 +0200

    Added functionality for the select

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,5 +19,27 @@ function updateTime() {
   );
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city"><div>
+              <h2>${cityName}</h2>
+              <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time"><strong>${cityTime.format(
+              "h:mm:ss"
+            )}</strong> <small>${cityTime.format("A")}</small>
+            </div>
+          </div>
+            </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
